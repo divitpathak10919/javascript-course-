fd7bfe5648308f3b8674648f5d02447c7f297226
divit <pathakdivit15@gmail.com>
2026-07-24 07:07:13 +0530

    updated JavaScript code

--- first.js
+++ first.js
@@ -4,5 +4,29 @@ price = 99.9;
 x=null;
 y=undefined;
 console.log(x);
+isfolllow=true;
+console.log(isfolllow);
+fullname=25;
+console.log(fullname);
+fullname="divit pathak";
+FULLNAME="divit pathak";
+console.log(fullname);
+letname="tony strak";
+console.log(letname);
+let age = 24;
 
+age = 25;
+age = 86;
+
+console.log(age);
+let age1 = 24;
+letfullname="tony stark";
+isfollow=true;
+const product = {
+    title: "ball pen",
+    rating: 4.5,
+    price: 217
+};
+
+console.log(product);
 
